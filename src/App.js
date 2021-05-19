a257881a46b8be496afad78be3c3cd8ac9a2c81e
Atala <atala.rchidi@gmail.com>
2021-05-19 12:53:29 -0400

    updated resume

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import { HashRouter, Route, Switch } from 'react-router-dom';
-import Contact from './pages/Contact';
-import Home from './pages/Home';
-import Skill from './pages/Skill';
-import NotFound from './pages/NotFound';
-import Project from './pages/Project';
-
+import React from "react";
+import { HashRouter, Route, Switch } from "react-router-dom";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Skill from "./pages/Skill";
+import NotFound from "./pages/NotFound";
+import Project from "./pages/Project";
 
 const App = () => {
   return (
-    <HashRouter basename='/'>
+    <HashRouter basename="/">
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/skills" component={Skill} />
--- src/components/project/Projects.js
+++ src/components/project/Projects.js
@@ -1,40 +1,53 @@
-import React from 'react';
-
+import React from "react";
 
 const Projects = (props) => {
   return (
-    <div className="projects"> {/*content*/}
-    
+    <div className="projects">
+      {" "}
+      {/*content*/}
       <div className="icons">
-        {props.items.languagesIcons.map(icon => 
-          <i className={icon} key={icon}></i> 
-        )}
+        {props.items.languagesIcons.map((icon) => (
+          <i className={icon} key={icon}></i>
+        ))}
       </div>
-
       <h3>{props.items.name}</h3>
+      <div className="card">
+        {/*photo*/}
 
-      <div className="card">{/*photo*/}
-
-        <img className="card__face card__face--front" src={props.items.picture} alt="imagePortfolio" />
+        <img
+          className="card__face card__face--front"
+          src={props.items.picture}
+          alt="imagePortfolio"
+        />
 
         <div className="card__face card__face--back">
           <div className="head">
-            {
-              props.items.demo && (
-                <div className="demoCode">
-                  <a href={props.items.demo} rel="noopener noreferrer" className="button" target="_blank">View demo</a>
-                </div>
-              )
-            }
+            {props.items.demo && (
+              <div className="demoCode">
+                <a
+                  href={props.items.demo}
+                  rel="noopener noreferrer"
+                  className="button"
+                  target="_blank"
+                >
+                  View demo
+                </a>
+              </div>
+            )}
             <div className="sourceCode">
-              <a href={props.items.source} rel="noopener noreferrer" className="button" target="_blank">Source code</a>
+              <a
+                href={props.items.source}
+                rel="noopener noreferrer"
+                className="button"
+                target="_blank"
+              >
+                Source code
+              </a>
             </div>
           </div>
           <div className="text">{props.items.info}</div>
-
         </div>
       </div>
-
     </div>
   );
 };
